62248df14728477148aad632784f31e5eb523d2e
Akshay Kumar Police Patil <akshaypolice373@gmail.com>
2021-05-26 15:52:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       items : []
+    }
+  }
+  componentDidMount() {
+    axios.get("https://reqres.in/api/users?page=2")
+    .then((res) => {
+      
+      this.setState({
+        items: res.data.data
+      })
+      console.log(this.state.items)
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <h1 className="header">Users List</h1>
+        <div className="row">
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>EmailId</th>
+              </tr>
+            </tbody>
+            <tbody>
+              { this.state.items.map(
+                  items =>
+                    <tr key="items.id">
+                      <td>{items.id}</td>
+                      <td>{items.first_name}</td>
+                      <td>{items.last_name}</td>
+                      <td>{items.email}</td>
+                    </tr>                  
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
